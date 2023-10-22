3d5db35e2c2527d12df58b685c5d1a1afea435d7
shantanu020 <shantanuytech@gmail.com>
2023-10-22 20:35:28 +0530

    Design

---
+++ Patterns/Design.java
@@ -0,0 +1,39 @@
+public class Design {
+    public static void main(String args[]) {
+        int n = 9;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1 || i == 0 || j == 0 || i == n - 1 || j == n - 1 || i + j == (n - 1) / 2
+                        || i - j == (n - 1) / 2 || (i + j) == (n - 1) * 3 / 2 || j - i == (n - 1) / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (i + j == (n - 1) / 2 || i - j == (n - 1) / 2 || j - i == (n - 1) / 2 || i + j == (n - 1) * 3 / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (i + j <= (n - 1) / 2 || i - j >= (n - 1) / 2 || j - i >= (n - 1) / 2 || i + j >= (n - 1) * 3 / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == (n - 1) / 2 || j == 0 && i < (n - 1) / 2 || j == n - 1 && i < (n - 1) / 2
+                        || i + j >= (n - 1) / 2 && i < (n - 1) / 2 && j <= (n - 1) / 2
+                        || j - i <= (n - 1) / 2 && i < (n - 1) / 2 && j >= (n - 1) / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
