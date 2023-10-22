cf161acaae043b7c680462c06a6ed04d33aa0fce
shantanu020 <shantanuytech@gmail.com>
2023-10-22 14:43:23 +0530

    Square pattern

---
+++ Patterns/Square.java
@@ -0,0 +1,11 @@
+public class Square{
+    public static void main(String args[]){
+    int n=9;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+}
